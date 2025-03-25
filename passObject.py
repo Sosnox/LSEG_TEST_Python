dfbc6058b951b38dbc7cdc94a583cb688728b03f
INTHAT SRIPHAO <86417460+Sosnox@users.noreply.github.com>
2025-03-25 22:16:25 +0700

    init code to Repository

---
+++ passObject.py
@@ -0,0 +1,21 @@
+def passObject(obj, key):
+    # Split key to loop value Object
+    listkey = key.split("/")
+    for i in listkey:
+        obj = obj[i]
+    return obj
+
+def readFileJson(jsonFile):
+    # Read File Json
+    with open(jsonFile, "r") as f:
+        jsonFile = f.read()
+    return eval(jsonFile)
+
+def main():
+    pathData = "data_2.json"
+    key = input("Enter key Format like : a/b/c ")
+    ObjectList =  readFileJson(pathData)
+    result = passObject(ObjectList, key)
+    print("Result:", result)
+
+main()
